1f9f2ee9c38d1e87c0231c641dec9164a36731fb
Kyle Stough <kstough00@gmail.com>
2020-07-29 21:34:30 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 def unsafe?(speed)
   if speed > 60
-    puts true"
+    puts true
   elsif speed < 40
-    puts "true"
+    puts true
   else speed > 40 && speed < 60
-    puts "false"
+    puts false
   end
 end
 
